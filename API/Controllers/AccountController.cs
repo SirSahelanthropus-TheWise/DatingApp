dc998c659cefdc07c31eb99fb17f1df5522774f0
Vikas <saivikasmaram@gmail.com>
2022-10-04 20:32:05 +0530

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -52,7 +52,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
             if (user == null)
                 return Unauthorized("Invalid username"); // Return Unauthorized if no password is entered
 
---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } // Use lowercase for some reason
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; } // Will automatically get it from GetAge() in AppUser class.
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
