7e972de64a54c115c4e81d4545adfec6b2c215f9
Vikas <saivikasmaram@gmail.com>
2022-10-29 15:17:31 +0530

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -37,5 +38,19 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(username);
         }
 
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user); // Using automapper to automatically map each of the properties
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
     }
 }
